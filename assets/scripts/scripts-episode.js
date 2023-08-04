163bb73ec6545ee9d6f9136a2aa1eeee5b2b7725
Matheus S. Oliveira <matheuskiller6@gmail.com>
2023-08-03 21:46:21 -0300

    episodes page and start feedbacks areas

--- assets/scripts/scripts-episode.js
+++ assets/scripts/scripts-episode.js
@@ -1,17 +1,83 @@
+function showLoading() {
+  var box = document.getElementById("container");
+  box.classList.add("hidden");
+
+  var btnContainer = document.getElementById("btnContainer");
+  btnContainer.classList.add("hidden");
+
+  var div = document.createElement("div");
+  div.classList.add("loader");
+
+  document.body.appendChild(div);
+  setTimeout(() => hideLoading(), 1500);
+}
+
+function hideLoading() {
+  var btnContainer = document.getElementById("btnContainer");
+  btnContainer.classList.remove("hidden");
+
+  var container = document.getElementById("container");
+  container.classList.remove("hidden");
+  const loadings = document.getElementsByClassName("loader");
+  if (loadings.length) {
+    loadings[0].remove();
+  }
+}
+
+var page = 1;
+function nextPage() {
+  showLoading();
+  page++;
+  getEpisodes();
+  if (page > 3) {
+    alert("this is the last page");
+    page--;
+  }
+
+  console.log(page);
+}
+
+function prevPage() {
+  page--;
+  if (page >= 1) {
+    showLoading();
+  }
+
+  getEpisodes();
+  if (page < 1) {
+    alert("this is the first page");
+    page++;
+  }
+  console.log(page);
+}
+
 function getEpisodes() {
-  var container = document.querySelector(".container");
-  // container.innerHTML = "";
-  fetch(`https://rickandmortyapi.com/api/episode`)
+  var container = document.querySelector(".epcontainer");
+  container.innerHTML = "";
+  fetch(`https://rickandmortyapi.com/api/episode?page=${page}`)
     .then((resp) => resp.json())
     .then((data) => {
       console.log(data);
       data.results.map((results) => {
-        container.innerHTML =
+        container.innerHTML +=
           `
-       <div> 
-       Name: ` +
+       <div class="data"> 
+        
+       <div>
+       Episode: ` +
+          results.episode +
+          `
+       </div>
+       <div>
+        Name: ` +
           results.name +
-          ` 
+          `
+        </div>
+         <div>
+         Launch date: ` +
+          results.air_date +
+          `
+         </div>
        </div>
        `;
       });
